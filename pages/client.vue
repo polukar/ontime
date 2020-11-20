0918986a8de755947f1d5736fedb1f0e9e6ca403
plukarov <ap@finfort.ru>
2020-11-20 15:20:37 +0300

    add enter page / add clients page

--- pages/client.vue
+++ pages/client.vue
@@ -4,7 +4,7 @@
     <div class="content">
       <header class="page__header d-flex justify-space-between align-center">
         <h1 class="h1-title">Клиенты</h1>
-        <v-btn fab class="btn-add">
+        <v-btn fab class="btn-add" @click="openPanel">
           <v-icon light> mdi-plus </v-icon>
         </v-btn>
       </header>
@@ -65,10 +65,57 @@
             </div>
           </template>
           <template v-slot:item.actions="{ item }">
-            <v-btn small outlined @click="editItem(item)"> Записи</v-btn>
-            <v-btn small outlined @click="editItem(item)"> Открыть</v-btn>
+            <v-btn small outlined @click="editItem"> Записи</v-btn>
+            <v-btn small outlined @click="editItem"> Открыть</v-btn>
           </template>
         </v-data-table>
+      </div>
+      <div class="panel" :class="{ active: panelActive }">
+        <div class="panel__head d-flex justify-space-between align-center">
+          <div class="back d-flex align-center" @click="closePanel"></div>
+          <div class="panel__title d-flex justify-center">Место</div>
+          <div class="close d-flex justify-end" @click="closePanel">
+            <svg-icon class="panel__head-ico" name="icon-close" />
+          </div>
+        </div>
+        <div class="panel__body">
+          <label class="panel-label">
+            <v-text-field label="Имя"></v-text-field>
+          </label>
+          <label class="panel-label">
+            <v-text-field label="Фамилия"></v-text-field>
+          </label>
+          <label class="panel-label">
+            <v-text-field label="Телефон"></v-text-field>
+          </label>
+          <label class="panel-label">
+            <v-text-field label="WhatsApp"></v-text-field>
+          </label>
+          <label class="panel-label">
+            <v-text-field label="Telegram"></v-text-field>
+          </label>
+          <label class="panel-label">
+            <v-text-field label="Цена за 60 мин"></v-text-field>
+          </label>
+          <label class="panel-label d-flex align-center">
+            <v-checkbox class="panel__checkbox"></v-checkbox>
+            Постоянный клиент
+          </label>
+          <label class="panel-label d-flex align-center">
+            <v-checkbox class="panel__checkbox"></v-checkbox>
+            Активно
+          </label>
+          <label class="panel-label">
+            <v-text-field label="Комментарий к месту"></v-text-field>
+          </label>
+          <div class="delete d-flex align-center" v-if="editOrNew">
+            <svg-icon class="panel__head-ico" name="icon-delete" />
+            Удалить место
+          </div>
+        </div>
+        <div class="panel__footer">
+          <v-btn normal class="btn-primary">СОХРАНИТЬ</v-btn>
+        </div>
       </div>
     </div>
   </div>
@@ -92,7 +139,20 @@ export default {
       clients: this.$store.state.clients.clients,
       active: "Все",
       actives: ["Все", "1", "2", "3"],
+      editOrNew: true,
+      panelActive: false,
     };
+  },
+  methods: {
+    openPanel() {
+      this.panelActive = !this.panelActive;
+    },
+    closePanel() {
+      this.panelActive = !this.panelActive;
+    },
+    editPanel() {
+      this.openPanel();
+    },
   },
 };
 </script>
---
+++ pages/enter.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="enter d-flex justify-center align-start">
+    <div class="enter__panel">
+      <div class="enter__logo">
+        <svg-icon name="logo" />
+      </div>
+      <v-tabs v-model="tab" class="enter__tabs">
+        <v-tabs-slider color="yellow"></v-tabs-slider>
+
+        <v-tab v-for="item in items" :key="item">
+          {{ item }}
+        </v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <div class="enter__socials d-flex justify-space-between align-center">
+            <span>Вы можете войти с помощью</span>
+            <div class="socials d-flex justify-end">
+              <a href="" class="socials__link">
+                <svg-icon name="icon-vk" />
+              </a>
+              <a href="" class="socials__link">
+                <svg-icon name="icon-facebook" />
+              </a>
+              <a href="" class="socials__link">
+                <svg-icon name="icon-google" />
+              </a>
+            </div>
+          </div>
+          <v-form v-model="valid" class="enter__form">
+            <div class="enter__input">
+              <input type="text" placeholder="Номер телефона или email" />
+            </div>
+            <div class="enter__input">
+              <input type="password" placeholder="Пароль" />
+            </div>
+            <div class="enter__btn">
+              <v-btn normal class="btn-primary">Войти</v-btn>
+            </div>
+          </v-form>
+        </v-tab-item>
+        <v-tab-item>
+          <v-form v-model="valid" class="enter__form">
+            <div class="registr --sms" v-if="regStatus == 'sms'">
+              <div class="enter__input">
+                <input type="text" placeholder="Номер телефона или email" />
+              </div>
+              <div class="enter__input">
+                <input type="text" placeholder="Код из смс" />
+              </div>
+              <div class="enter__btn">
+                <v-btn normal @click="nextPassword" class="btn-primary"
+                  >Продолжить</v-btn
+                >
+              </div>
+            </div>
+            <div class="registr --password" v-else-if="regStatus == 'password'">
+              <div class="enter__input">
+                <input type="password" placeholder="Создайте пароль" />
+              </div>
+              <div class="enter__input">
+                <input type="password" placeholder="Повторите пароль " />
+              </div>
+              <div class="enter__btn">
+                <v-btn normal class="btn-primary">Регистрация</v-btn>
+              </div>
+            </div>
+            <div class="registr" v-else>
+              <div class="enter__input">
+                <input type="text" placeholder="Номер телефона или email" />
+              </div>
+              <div class="enter__btn">
+                <v-btn normal class="btn-primary" @click="nextSms"
+                  >Продолжить</v-btn
+                >
+              </div>
+            </div>
+          </v-form>
+        </v-tab-item>
+      </v-tabs-items>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      tab: null,
+      items: ["Войти", "Регистрация"],
+      text: "123",
+      regStatus: "",
+    };
+  },
+  methods: {
+    nextSms() {
+      this.regStatus = "sms";
+    },
+
+    nextPassword() {
+      this.regStatus = "password";
+    },
+  },
+};
+</script>
